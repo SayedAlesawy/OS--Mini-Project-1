2b5b83f9dd0e7089130426017a6a85c42660a0dd
kareem <kareememad400@gmail.com>
2019-03-11 23:06:40 +0200

    package folder structure

--- main.go
+++ main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"OS--Mini-Project-1/quicksort"
 	"OS--Mini-Project-1/fileIO"
+	"OS--Mini-Project-1/quicksort"
+	"fmt"
 )
 
 //main main function
-func main(){
+func main() {
 	var inputFileName, outputFileName string
-	
+	var testInputSize string
 	fmt.Print("Input file name: ")
 	fmt.Scanf("%s\n", &inputFileName)
-	
+
 	fmt.Print("Output file name: ")
 	fmt.Scanf("%s\n", &outputFileName)
 
 	fmt.Print("Test array size: ")
 	fmt.Scanf("%d\n", &testInputSize)
 
-	intputArr := ioFiles.ReadFile(inputFileName)
+	intputArr := fileIO.ReadFile(inputFileName)
 
 	quicksort.Sort(intputArr, 0, len(intputArr))
 
-	ioFiles.WriteFile(intputArr, outputFileName)
+	fileIO.WriteFile(intputArr, outputFileName)
 
 	fmt.Println("Done sorting")
 }
